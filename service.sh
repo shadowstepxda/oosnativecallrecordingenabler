9dca8c53ea653482c8261e81a86c25793414135a
Pg <pg.developper.fr@gmail.com>
2020-05-31 10:41:48 +0200

    Improve timer for settings service
    The settings service is not available at the beginning of late_start because services are launched in this mode. Waiting 10s will be enough in most cases. But this is a race condition and it can be lost sometimes. In order to improve the chances without a waiting time too long for the user, a watcher is added to get when settings service is ready. The watcher timeout after 2min.

--- service.sh
+++ service.sh
@@ -1,4 +1,20 @@
 #!/system/bin/sh
 # This script will be executed in late_start service mode
-(sleep 10
-settings put global op_voice_recording_supported_by_mcc 1)&
+
+# wait until the settings service is available
+i=0
+settings > /dev/null 2>&1
+until [ $? -eq 255 ]
+do
+    sleep 5
+    i=$((i+1))
+    # if settings is not ready after 2min, stop
+    if [ $i -gt 24 ]
+    then
+        exit 1
+    fi
+
+    settings > /dev/null 2>&1
+done
+
+settings put global op_voice_recording_supported_by_mcc 1
